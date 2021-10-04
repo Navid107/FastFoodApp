01231e1e326bce4b7b107a7242bcc692b2fa7312
Mohammad Yazdani <mohammadyazdani@Mohammads-MacBook-Air.local>
2021-10-04 15:30:05 -0700

    fix-model

--- models/food.js
+++ models/food.js
@@ -1,10 +1,17 @@
 const mongoose = require('mongoose');
 
-const orderSchema = new mongoose.Schema({
-   burgerValue: Number,
-   FriesValue: Number,
-   sodaValue: Number,
-    user: {type: mongoose.Schema.Types.ObjectId, ref: 'User'}
-  })
-
+const reviewSchema = new mongoose.Schema({
+    content: String,
+    rating: Number,
+     user: {type: mongoose.Schema.Types.ObjectId, ref: 'User'}
+   })
+ 
+ 
+ const orderSchema = new mongoose.Schema({
+    burgerValue: Number,
+    friesValue: Number,
+    sodaValue: Number,
+     user: {type: mongoose.Schema.Types.ObjectId, ref: 'User'},
+     reviews: [reviewSchema]
+   })
   module.exports = mongoose.model('Order', orderSchema);
--- models/user.js
+++ models/user.js
@@ -1,11 +1,5 @@
 const mongoose = require('mongoose');
 
-const reviewSchema = new mongoose.Schema({
-	content: String,
-  
-  }, {
-	timestamps: true
-  });
 
 // Create your User Model
 const userSchema = new mongoose.Schema({
@@ -15,12 +9,7 @@ const userSchema = new mongoose.Schema({
   },{
     timestamps: true
   });
-const foodSchema = new mongoose.Schema({
-   
-    name: String,
-    value: Number,
-    review: [reviewSchema]
-  })
+
 
 // need to have a googleId on student 
 module.exports = mongoose.model('User', userSchema);
